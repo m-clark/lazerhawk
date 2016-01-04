3ef004c581d9cdeb64a92d8548ca912e381e3706
Clark <micl@UMICH.EDU>
2016-01-04 16:11:22 -0500

    trying out addin functionality

---
+++ R/addins.R
@@ -0,0 +1,20 @@
+#' Return an img tag for a centered image
+#'
+#' @description Inserts html for a centered image
+#'
+#'
+#'
+#' @details This function is an Rstudio addin that inserts <img src="" style="display:block; margin: 0 auto;"> so that one can have a centered image on the fly.
+#'
+#'
+#' @return <img src="" style="display:block; margin: 0 auto;"> as text.
+#'
+#' @examples
+#' ## Not Run
+#' # library(lazerhawk)
+#' # insertImgCenterAddin()
+#'
+#' @export
+insertImgCenterAddin <- function() {
+  rstudioapi::insertText("<img src=\"\" style=\"display:block; margin: 0 auto;\">")
+}
