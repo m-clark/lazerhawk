ccc76429c3bcd17c9107a2a3c4579fafe2b9a077
mc <6876c76f@opayq.com>
2017-03-31 19:47:59 -0400

    remove corrheat, add sum missing.

---
+++ R/sum_NA.R
@@ -0,0 +1,42 @@
+#' Investigate missingness
+#'
+#' @param x a vector with multiple elements
+#'
+#' @details These functions return the number of NA, NaN, or blank values in a
+#'   vector.  Such information can serve as a diagnostic or used outright, e.g.
+#'   in longitudinal settings. If you are dealing with singletons, you should
+#'   just use the underlying function, e.g. is.na. For total blank values, the
+#'   stringi package is required.
+#'
+#' @return The number of non-elements so defined
+#'
+#' @examples
+#' library(lazerhawk)
+#' sum_blank(c('    ', '', 'c', 'd'))
+#' sum_NA(c(NA, '', 'c', NA))
+#' sum_NaN(c(1:3, NA, NaN))
+#' sum_NA(1:4)
+#'
+#'
+#'
+#' @export
+sum_NA <- function(x) {
+  if(length(x)==1) stop('Just look at it you dolt.')
+  sum(is.na(x))
+}
+
+#' @rdname sum_NA
+#' @export
+sum_NaN <- function(x) {
+  if(length(x)==1) stop('Just look at it you dolt.')
+  if(!is.numeric(x)) stop('x must potentially be a number in order to not be a number. ~ The Buddha')
+  sum(is.nan(x))
+}
+
+#' @rdname sum_NA
+#' @export
+sum_blank <- function(x) {
+  if(length(x)==1) stop('Just look at it you dolt.')
+  sum(stringi::stri_trim_both(x)=='')
+}
+
