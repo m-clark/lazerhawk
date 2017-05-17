676ebdfaaca4972c97c667a606ede1c97f8faf4c
m-clark <micl@umich.edu>
2017-05-17 19:49:56 -0400

    add palettes

---
+++ R/palettes.R
@@ -0,0 +1,13 @@
+#' Commonly used palettes
+#'
+#' A list of commonly used palettes with some base color and including complementary, monochromatic, analagous, split_complementary, triadic, tetradic.  Otherwise, just ones I use (e.g. colorbrewer)
+#'
+#' @format (Possibly) named vectors/lists of colors
+#'
+#' - orange : #ff5503
+#'
+#' - RdBu_11: RdBu palette from colorbrewer for sequence of 11
+#'
+#' ...
+#' @source For example \url{http://paletton.com/}, \url{https://www.sessions.edu/color-calculator/},  \url{https://colorbrewer.org}
+'palettes'
