43ca433bbb6ca386ffcfdb47bcec671184e21a52
Michael Clark <micl@umich.edu>
2017-06-09 16:31:01 -0400

    add tests

--- tests/testthat/test_lowerTri.R
+++ tests/testthat/test_lowerTri.R
@@ -1,14 +1,28 @@
 context('lowerTri error for data.frame input')
 test_that('Error returned for data.frame input', {
-  expect_error(lowerTri(data.frame(matrix(1:9))))
+  expect_error(lowerTri(data.frame(matrix(1:9, 3, 3))))
 })
 
-context('lowerTri error for non-square input')
 test_that('Error returned for non-square input', {
   expect_error(lowerTri(matrix(1:10, ncol=5)))
 })
 
-context('lowerTri error for non-numeric input of otherVal argument')
 test_that('Error returned for non-numeric input', {
   expect_error(lowerTri(matrix(1:9, ncol=3), otherVal = 'A'))
+})
+
+test_that('lowerTri returns matrix.', {
+  expect_true(is.matrix(lowerTri(matrix(1:9, ncol=3))))
+})
+
+test_that('lowerTri returns vector.', {
+  expect_true(is.numeric(lowerTri(matrix(1:9, ncol=3), valuesOnly = TRUE)))
+})
+
+test_that('upperTri returns matrix.', {
+  expect_true(is.matrix(upperTri(matrix(1:9, ncol=3))))
+})
+
+test_that('upperTri returns vector.', {
+  expect_true(is.numeric(upperTri(matrix(1:9, ncol=3), valuesOnly = TRUE)))
 })
