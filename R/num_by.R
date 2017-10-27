f27fb22ada104de5a5c3d29f2a49175a054865f9
m-clark <micl@umich.edu>
2017-10-27 08:43:20 -0400

    Add n and quartiles to num_by, as well as tests

--- R/num_by.R
+++ R/num_by.R
@@ -1,14 +1,18 @@
 #' Summarize data
 #'
+#' @description Provide common numeric summary information.
+#'
 #' @param df data frame
 #' @param main_var the main numeric variable to be summarised
 #' @param group_var the optional grouping variable
 #'
 #' @details This function takes a numeric variable from a dataframe and provides
-#'   mean, standard deviation, median, min, and max, possibly over a grouping
-#'   variable. It works in the dplyr style using unquoted (bare) variable names.
+#'   sample size, mean, standard deviation, min, first quartile, median, third
+#'   quartile, and max, possibly over a grouping variable. It works in the dplyr
+#'   style using unquoted (bare) variable names.
 #' @return data.frame/tibble with the corresponding summary statistics
 #' @seealso describeAll
+#' @importFrom stats quantile
 #' @examples
 #' library(dplyr)
 #' df1 <- tibble(
@@ -28,6 +32,8 @@
 #'
 #' @export
 num_by <- function(df, main_var, group_var) {
+  if (nrow(df)==0) stop('No data to summarise.')
+
   mv <- enquo(main_var)
   if (!class(df %>% pull(!!mv)) %in% c('numeric', 'integer', 'logical')) stop('Need numeric variable for main_var.')
 
@@ -36,10 +42,13 @@ num_by <- function(df, main_var, group_var) {
     df %>%
       group_by(!!gv) %>%
       summarise(
+        N = n()-sum(is.na(!!mv)),               # this was faster on 1mil observations than several alternatives
         Mean = mean(!!mv, na.rm = TRUE),
         SD = sd(!!mv, na.rm = TRUE),
-        Median = median(!!mv, na.rm = TRUE),
         Min = min(!!mv, na.rm = TRUE),
+        Q1 = quantile(!!mv, p=.25, na.rm = TRUE),
+        Median = median(!!mv, na.rm = TRUE),
+        Q3 = quantile(!!mv, p=.75, na.rm = TRUE),
         Max = max(!!mv, na.rm = TRUE)
       ) %>%
       mutate(Variable = c(quo_name(mv), rep('', nrow(.)-1))) %>%
@@ -47,10 +56,13 @@ num_by <- function(df, main_var, group_var) {
   } else {
     df %>%
       summarise(
+        N = n()-sum(is.na(!!mv)),
         Mean = mean(!!mv, na.rm = TRUE),
         SD = sd(!!mv, na.rm = TRUE),
-        Median = median(!!mv, na.rm = TRUE),
         Min = min(!!mv, na.rm = TRUE),
+        Q1 = quantile(!!mv, p=.25, na.rm = TRUE),
+        Median = median(!!mv, na.rm = TRUE),
+        Q3 = quantile(!!mv, p=.75, na.rm = TRUE),
         Max = max(!!mv, na.rm = TRUE)
       ) %>%
       mutate(Variable = quo_name(mv)) %>%
---
+++ tests/testthat/test_num_by.R
@@ -0,0 +1,25 @@
+context('test num_by')
+
+df1 <- tibble(
+  g1 = sample(1:2, 50, replace = TRUE),
+  g2 = sample(1:4, 50, replace = TRUE),
+  a = rnorm(50),
+  b = rpois(50, 10),
+  c = sample(letters, 50, replace=TRUE),
+  d = sample(c(T,F), 50, replace=TRUE)
+)
+test_that('num_by returns a data frame', {
+  expect_s3_class(num_by(df1, main_var = a), 'data.frame')
+})
+
+test_that('num_by takes a group var', {
+  expect_s3_class(num_by(df1, main_var = a, group_var = g2), 'data.frame')
+})
+
+test_that('fails on non-numeric', {
+  expect_error(num_by(df1, main_var = c))
+})
+
+test_that('num_by is ok with logical', {
+  expect_s3_class(num_by(df1, main_var = d, group_var = g2), 'data.frame')
+})
