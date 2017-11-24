b83e23564a0c348f86fd8155b21f9a20cf967399
m-clark <micl@umich.edu>
2017-11-24 12:11:09 -0500

    update to numby to add multiple variables

--- R/num_by.R
+++ R/num_by.R
@@ -3,24 +3,31 @@
 #' @description Provide common numeric summary information.
 #'
 #' @param df data frame
-#' @param main_var the main numeric variable to be summarised
-#' @param group_var the optional grouping variable
+#' @param main_var the variable to be summarised. For num_by, this can be
+#'   multiple variables using the vars() function.
+#' @param group_var the (optional) grouping variable
 #' @param perc_by_group when supplied a grouping variable for cat_by, do you
 #'   want within group percentages (default) or out of total?
+#' @param sort_by_group when supplied a grouping variable for cat_by, do you
+#'   want result sorted on the grouping variable? Default is TRUE.
 #' @param digits optional rounding
 #'
 #' @details The \code{num_by} function takes a numeric variable from a dataframe
 #'   and provides sample size, mean, standard deviation, min, first quartile,
-#'   median, third quartile, and max, possibly over a grouping variable. It
-#'   works in the dplyr style using unquoted (bare) variable names.
+#'   median, third quartile, max, and number of missing values, possibly over a
+#'   grouping variable. It works in the dplyr style using unquoted (bare)
+#'   variable names, using the vars() function if there is more than one
+#'   variable.  If using a grouping variable, it will treat missing values as a
+#'   separate group.
 #'
 #'   For \code{cat_by}, frequencies and percentage (within the grouping variable
 #'   or out of total) are returned. Warnings are given if there are more than 10
-#'   levels or the data appears to be numeric.
+#'   levels or the data appears to be numeric. It will only work on a one or two (if group_var is supplied) categorical variables, as defining what percentages should be calculated is not obvious.
 #'
 #' @return data.frame/tibble with the corresponding summary statistics
 #' @seealso describeAll
 #' @importFrom stats quantile
+#' @importFrom rlang is_quosures
 #' @examples
 #' df1 <- data.frame(g1 = factor(sample(1:2, 50, replace = TRUE), labels=c('a','b')),
 #'                   g2 = sample(1:4, 50, replace = TRUE),
@@ -32,52 +39,112 @@
 #'
 #'
 #' num_by(df1, main_var = a)
-#' num_by(df1, main_var = a, group_var = g2)
+#' num_by(df1, main_var = a, group_var = g2, digits=2)
 #'
-#' num_by(df1, main_var = b)
-#' num_by(df1, main_var = b, group_var = g1, digits=2)
+#' num_by(df1, main_var = dplyr::vars(a,b), group_var = g1, digits=1)
 #'
 #' cat_by(df1, main_var = g1, group_var = g2, digits=1)
 #' cat_by(df1, main_var = g1, group_var = g2, perc_by_group=FALSE)
 #'
 #' @export
 num_by <- function(df, main_var, group_var, digits=FALSE) {
-  if (nrow(df)==0) stop('No data to summarise.')
+  if (nrow(df)==0 | is.null(df)) stop('No data to summarise.')
 
-  mv <- enquo(main_var)
-  if (!class(df %>% pull(!!mv)) %in% c('numeric', 'integer', 'logical')) stop('Need numeric variable for main_var.')
+  # for future reference; the tryCatch was just to make it easy to pass a single
+  # variable name in lieu of using vars().
+  # - summarise_* won't take single unquoted names nor is there a straightforward way to pass
+  # one or even check if one is being passed.
+  # - summarise_* also won't take an unquoted varname to vars, which is just a
+  # wrapper to the quos function
+  # - variants of vars do not return the same class object as vars()
+  #
+  # Other stuff:
+  # you can't do !!enquo(x); you have to do z = enquo(x); !!z or !(!enquo(x))
+  # though the latter likely would only work within a function
+  # in retrospect, mutate_at + distinct would have likely worked better, but
+  # distinct won't take a vars input, because why would it?
+  # perhaps tidyselect will have some useful documentation in the future
+
+  # Initial checks ------------------------------------------------------------
+  if (!'data.frame' %in% class(df)) stop('Need a data.frame type object.')
+
+
+  # section to deal with single variable name -------------------------------
+  mv = enquo(main_var)  # has to be here or errs with 'has already been evaluated'
+
+  x = tryCatch(rlang::is_quosures(main_var), error = function(c) {
+    msg = conditionMessage(c)
+    invisible(structure(msg, class = "try-error"))
+  })
+
+  if (class(x) == 'try-error') main_var = quos(!!mv)
+
+  class_mv = df %>%
+    summarise_at(main_var, funs(cls=class(.))) %>%
+    unlist()
+  if (!all(class_mv %in% c('numeric', 'integer', 'logical'))) stop('Non-numeric/logical variable detected.')
+
+
+  # Main processing ---------------------------------------------------------
 
   if (!rlang::quo_is_missing(enquo(group_var))) {
-    gv <- enquo(group_var)
-    df= df %>%
+
+    #• grouped result ----------------------------------------------------------
+
+    gv = enquo(group_var)
+    df = df %>%
+      # filter(!is.na(!!gv)) %>%
       group_by(!!gv) %>%
-      summarise(
-        N = n()-sum(is.na(!!mv)),               # this was faster on 1mil observations than several alternatives
-        Mean = mean(!!mv, na.rm = TRUE),
-        SD = sd(!!mv, na.rm = TRUE),
-        Min = min(!!mv, na.rm = TRUE),
-        Q1 = quantile(!!mv, p=.25, na.rm = TRUE),
-        Median = median(!!mv, na.rm = TRUE),
-        Q3 = quantile(!!mv, p=.75, na.rm = TRUE),
-        Max = max(!!mv, na.rm = TRUE)
-      ) %>%
-      mutate(Variable = c(quo_name(mv), rep('', nrow(.)-1))) %>%
-      select(Variable, everything())
+      summarise_at(main_var,
+                   funs(
+                     N = n()-sum(is.na(.)),           # this was faster on 1mil observations than several alternatives
+                     Mean = mean(., na.rm = TRUE),
+                     SD = sd(., na.rm = TRUE),
+                     Min = min(., na.rm = TRUE),
+                     Q1 = quantile(., p=.25, na.rm = TRUE),
+                     Median = median(., na.rm = TRUE),
+                     Q3 = quantile(., p=.75, na.rm = TRUE),
+                     Max = max(., na.rm = TRUE),
+                     Missing = ifelse(any(is.na(.)), sum(is.na(.)), NA)
+                   )
+      )
+    if (length(main_var) > 1) {
+      df = df %>%
+        tidyr::gather(key=results, value=value, -!!gv) %>%
+        tidyr::separate(col=results, sep='_', into=c('Variable', 'result')) %>%
+        tidyr::spread(result, value) %>%   # the obsession with auto alphabetical order in the tidyverse strikes once again
+        select(!!gv, Variable, N,  Mean, SD, Min, Q1, Median, Q3, Max, Missing)
+    }
   } else {
-    df= df %>%
-      summarise(
-        N = n()-sum(is.na(!!mv)),
-        Mean = mean(!!mv, na.rm = TRUE),
-        SD = sd(!!mv, na.rm = TRUE),
-        Min = min(!!mv, na.rm = TRUE),
-        Q1 = quantile(!!mv, p=.25, na.rm = TRUE),
-        Median = median(!!mv, na.rm = TRUE),
-        Q3 = quantile(!!mv, p=.75, na.rm = TRUE),
-        Max = max(!!mv, na.rm = TRUE)
-      ) %>%
-      mutate(Variable = quo_name(mv)) %>%
-      select(Variable, everything())
+
+    #• non-grouped result ------------------------------------------------------
+
+    df = df %>%
+      summarise_at(main_var,
+                   funs(
+                     N = n()-sum(is.na(.)),
+                     Mean = mean(., na.rm = TRUE),
+                     SD = sd(., na.rm = TRUE),
+                     Min = min(., na.rm = TRUE),
+                     Q1 = quantile(., p=.25, na.rm = TRUE),
+                     Median = median(., na.rm = TRUE),
+                     Q3 = quantile(., p=.75, na.rm = TRUE),
+                     Max = max(., na.rm = TRUE),
+                     Missing = ifelse(any(is.na(.)), sum(is.na(.)), NA)
+                   )
+      )
+    if (length(main_var) > 1) {
+      # suppress warnings if mixed logical and numerics
+      suppressWarnings({
+        df = df %>%
+          tidyr::gather(key=results, value=value) %>%
+          tidyr::separate(col=results, sep='_', into=c('Variable', 'result')) %>%
+          tidyr::spread(result, value) %>%
+          select(Variable, N,  Mean, SD, Min, Q1, Median, Q3, Max, Missing)
+      })
+    }
   }
+
   if (digits) df = df %>% mutate_if(is.numeric, round, digits=digits)
   df
 }
@@ -87,7 +154,7 @@ num_by <- function(df, main_var, group_var, digits=FALSE) {
 #' @rdname num_by
 #' @export
 #'
-cat_by <- function(df, main_var, group_var, digits=FALSE, perc_by_group=TRUE) {
+cat_by <- function(df, main_var, group_var, digits=FALSE, perc_by_group=TRUE, sort_by_group=TRUE) {
   if (nrow(df)==0) stop('No data to summarise.')
 
   mv <- enquo(main_var)
@@ -107,6 +174,11 @@ cat_by <- function(df, main_var, group_var, digits=FALSE, perc_by_group=TRUE) {
       df = df %>%
         ungroup() %>%
         mutate(`%` = 100*N/sum(N))
+    }
+    if (sort_by_group){
+      df = df %>%
+        select(!!gv, everything()) %>%
+        arrange(!!gv)
     }
   } else {
     df = df %>%
