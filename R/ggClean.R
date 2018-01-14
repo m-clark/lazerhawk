6479cba54edb616949d3e9fc8582a4c0e4d49fdc
Michael Clark <micl@umich.edu>
2018-01-14 18:33:48 -0500

    sigh

--- R/ggClean.R
+++ R/ggClean.R
@@ -19,7 +19,7 @@
 #' require(lazerhawk)
 #' require(ggplot2)
 #' data(mtcars)
-#' mtcars %>% ggplot(aes(wt, mpg)) + theme_trueMinimal()
+#' ggplot(aes(wt, mpg), data=mtcars) + theme_trueMinimal()
 #' require(plotly)
 #' mtcars %>% plot_ly(x=~wt, y=~mpg, mode='markers') %>% theme_plotly()
 #' mtcars %>% plot_ly(x=~wt, y=~mpg, mode='markers') %>% theme_blank()
