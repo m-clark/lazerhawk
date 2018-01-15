6ae124c095add805e17f0c8aa55dfd051a995036
Michael Clark <micl@umich.edu>
2018-01-14 19:10:05 -0500

    see previous

--- R/ggClean.R
+++ R/ggClean.R
@@ -19,7 +19,7 @@
 #' require(lazerhawk)
 #' require(ggplot2)
 #' data(mtcars)
-#' ggplot(aes(wt, mpg), data=mtcars)# + theme_trueMinimal()
+#' #ggplot(aes(wt, mpg), data=mtcars) + theme_trueMinimal() # currently ggplot is having a problem with rlang so this is commented until they fix it
 #' require(plotly)
 #' mtcars %>% plot_ly(x=~wt, y=~mpg, mode='markers') %>% theme_plotly()
 #' mtcars %>% plot_ly(x=~wt, y=~mpg, mode='markers') %>% theme_blank()
