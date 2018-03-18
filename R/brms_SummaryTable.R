22d24091bbd7407b432f9cbd812a051411b6a589
Michael Clark <micl@umich.edu>
2018-03-18 19:17:40 -0400

    update brms_SummaryTable as planned many moons ago.

--- R/brms_SummaryTable.R
+++ R/brms_SummaryTable.R
@@ -5,8 +5,8 @@
 #'
 #'
 #' @param model A brmsfit object
-#' @param formatOptions A list(!) of options to pass to the format function
-#' @param round See code{\link[base]{round}}
+#' @param formatOptions A named list(!) of options to pass to the format function
+#' @param round See \code{\link[base]{round}}
 #' @param astrology Put asterisks next to effects whose credible interval does not
 #'   contain 0
 #' @param hype Run one-sided hypothesis tests of whether effect is greater
@@ -14,6 +14,7 @@
 #' @param panderize Create a markdown summary table. Requires the pander package
 #' @param justify Additional argument to pass to \code{\link[pander]{pander}}
 #' @param ... Additional arguments to pass to \code{\link[brms]{summary.brmsfit}}
+#' @param seed Seed for hypothesis tests
 #'
 #' @details This function creates a data.frame summary object for a brms package
 #'   model object.  As of now it only does so for the fixed effects part of the
@@ -48,72 +49,72 @@
 #'
 #' library(lazerhawk)
 #' brms_SummaryTable(fit4)
+#' brms_SummaryTable(fit4, formatOptions=list(digits=5, nmsall=5))
 #' brms_SummaryTable(fit4, astrology=TRUE, hype=TRUE)
 #' brms_SummaryTable(fit4, astrology=TRUE, hype=TRUE, panderize=TRUE, justify='lrrrrclr')
 #' }
 #'
 #' @export
-brms_SummaryTable <- function(model, formatOptions=list(digits=2, nsmall=2), round=2,
-                              astrology=F, hype=F, panderize=F, justify=NULL, ...) {
+brms_SummaryTable <- function(model,
+                              formatOptions=list(digits=2, nsmall=2),
+                              round=2,
+                              astrology=FALSE,
+                              hype=FALSE,
+                              panderize=FALSE,
+                              justify=NULL,
+                              seed = 1234,
+                              ...) {
   if (!requireNamespace("brms", quietly = TRUE)) {
     stop("brms package, along with related dependencies, is needed for this function to work. Please install it.",
          call. = FALSE)
   }
 
-  if(class(model) != "brmsfit") stop('Model is not a brmsfit class object.')
+  if (!inherits(model, "brmsfit")) stop('Model is not a brmsfit class object.')
 
-  est = broom::tidy(model)
-  fe_names0 = stringr::str_subset(est$term, pattern='^b(|cs|mo|me|m)_')
-  fe_names = stringr::str_replace(fe_names0, pattern='b_', '')  # won't generalize to others, but fine for now
-  partables = est[est$term %in% fe_names0, c('estimate', 'std.error', 'lower', 'upper')]
-  partables_formatted = data.frame(Covariate=fe_names,
-                                   do.call(format, list(x=round(partables, round), formatOptions[[1]])))
+  fe = brms::fixef(model)
+  partables_formatted = data.frame(Covariate=rownames(fe),
+                                   do.call(format, list(x=round(fe, round), unlist(formatOptions))))
+  rownames(partables_formatted) = NULL
+  colnames(partables_formatted)[4:5] = c('l-95% CI', 'u-95% CI')
 
-  if(!astrology & !hype) {
-    colnames(partables_formatted)[-1] = c('Estimate', 'Est.Error', 'l-95% CI', 'u-95% CI')
-    if (is.null(justify)) justify = paste0('l', paste0(rep('r', ncol(partables_formatted)-1), collapse = ''))
-    if (panderize) return(pander::pander(partables_formatted, justify='lrrrr', split.tables=Inf))
+  if(!astrology & !hype & !panderize) {
     return(partables_formatted)
   }
 
-  # star intervals not containing zero
-  if (astrology) {
-    sigeffects0 = apply(sign(partables[,c('lower', 'upper')]), 1,
-                        function(interval) ifelse(diff(interval)==0, '*', ''))
-    sigeffects =  data.frame(partables_formatted, sigeffects0)
-    colnames(sigeffects) = c('Covariate', 'Estimate', 'Est.Error', 'l-95% CI', 'u-95% CI', 'Notable')
-  } else {
-    sigeffects =  partables_formatted
-    colnames(sigeffects) = c('Covariate', 'Estimate', 'Est.Error', 'l-95% CI', 'u-95% CI')
-  }
-
-
   # conduct hypothesis tests
   if (hype) {
-    signcoefs = sign(partables[,'estimate'])
     testnams = mapply(function(coefname, sign)
-      ifelse(sign>0, paste0(coefname, ' > 0'), paste0(coefname, ' < 0')), fe_names, signcoefs)
-    hypetests = sapply(testnams, brms::hypothesis, x=model, simplify = F, alpha=.025, seed=NULL)
-    ER = sapply(hypetests, function(test) test[['hypothesis']]['Evid.Ratio'])
-    ER = unlist(ER)
-    sigeffects$pvals = ER/(ER+1)
-    sigeffects$pvals[is.infinite(ER)] = 1
-    sigeffects$pvals = do.call(format, list(x=round(sigeffects$pvals, round), formatOptions[[1]]))
-
-    colnames(sigeffects)[ncol(sigeffects)] = 'B < > 0'
-    sigeffects[,'Evidence Ratio'] = do.call(format, list(x=round(ER, round), formatOptions[[1]]))
+      ifelse(sign>0, paste0(coefname, ' > 0'), paste0(coefname, ' < 0')), rownames(fe), sign(fe[,'Estimate']))
+    hypetests = brms::hypothesis(x=model, testnams, seed=seed)
+    ER = hypetests$hypothesis$Evid.Ratio
+    partables_formatted$pvals = ER/(ER+1)
+    partables_formatted$pvals[is.infinite(ER)] = 1
+    partables_formatted$pvals = do.call(format, list(x=round(partables_formatted$pvals, round), formatOptions[[1]]))
+    colnames(partables_formatted)[ncol(partables_formatted)] = 'B < > 0'
+    partables_formatted[,'Evidence Ratio'] = do.call(format, list(x=round(ER, round), formatOptions[[1]]))
   }
 
-  if (astrology && hype)  sigeffects[, 'Notable'] = ifelse(as.numeric(sigeffects[,'Evidence Ratio']) >= 19, '*', '')
-
-  rownames(sigeffects) = NULL
+  # if hype, make star based on brms result, else interval
+  if (astrology && hype)  {
+    partables_formatted$Notable = hypetests$hypothesis$Star
+  } else if (astrology) {
+    partables_formatted$Notable =  apply(sign(fe[,c('2.5%ile', '97.5%ile')]),
+                                         1,
+                                         function(interval) ifelse(diff(interval)==0, '*', ''))
+  }
 
   if (panderize) {
-    if (is.null(justify)) justify = paste0('l', paste0(rep('r', ncol(sigeffects)-1), collapse = ''))
-    return(pander::pander(x=sigeffects, justify=justify, split.tables=Inf))
+    if (is.null(justify)) {
+      if (astrology) {
+        justify = paste0('l', paste0(rep('r', ncol(partables_formatted)-2), collapse = ''), 'c')
+      } else {
+        justify = paste0('l', paste0(rep('r', ncol(partables_formatted)-1), collapse = ''))
+      }
+    }
+    return(pander::pander(x=partables_formatted, justify=justify, split.tables=Inf))
   }
 
-  sigeffects
+  partables_formatted
 }
 
 
--- tests/testthat/test_brmsSummaryTable.R
+++ tests/testthat/test_brmsSummaryTable.R
@@ -1,22 +1,52 @@
-context('test brmsSummaryTable')
+context('test brms_SummaryTable')
 
-test_that('brmsSummaryTable returns a data frame', {
+test_that('brms_SummaryTable returns a data frame by default', {
   expect_s3_class(brms_SummaryTable(fit1), 'data.frame')
 })
 
-test_that('brmsSummaryTable can see the stars', {
-  expect_match(levels(brms_SummaryTable(fit1, astrology = TRUE)$Notable), '*')
+test_that('brms_SummaryTable can round', {
+  rounded = brms_SummaryTable(fit1, round=4)
+  expect_equal(nchar(as.character(rounded$Estimate)[1]), 6)
 })
 
-test_that('brmsSummaryTable can do ratios', {
-  expect_identical(!is.null(brms_SummaryTable(fit1, astrology = F, hype = TRUE)$`Evidence Ratio`), TRUE)
+test_that('brms_SummaryTable can see the stars', {
+  expect_match(levels(factor(brms_SummaryTable(fit1, astrology = TRUE)$Notable)), '*')
 })
 
-test_that('brmsSummaryTable can do both', {
-  expect_identical(!is.null(brms_SummaryTable(fit1, astrology = TRUE, hype = TRUE)$`Evidence Ratio`), TRUE)
+test_that('brms_SummaryTable can do ratios', {
+  expect_equal(ncol(brms_SummaryTable(fit1, hype = TRUE)), 7)
+})
+
+test_that('brms_SummaryTable can do both', {
+  expect_equal(ncol(brms_SummaryTable(fit1, astrology = TRUE, hype = TRUE)), 8)
+})
+
+test_that('brms_SummaryTable can panderize', {
+  # file.remove('brms_pander_output') # if you need to reset
+  expect_known_output(brms_SummaryTable(fit1, panderize = TRUE), 'brms_pander_output', print = TRUE)
 })
 
 
-test_that('brmsSummaryTable can justify', {
-  expect_is(brms_SummaryTable(fit1, astrology = TRUE, hype = TRUE, panderize = T, justify = 'lrrrrrrr'), 'NULL')
+test_that('brms_SummaryTable can justify', {
+  # file.remove('brms_pander_output') # if you need to reset
+  expect_known_output(brms_SummaryTable(fit1, panderize = TRUE, justify = 'lrrrr'),
+                      'brms_pander_output1',
+                      print = TRUE)
+})
+
+
+test_that('brms_SummaryTable can justify with hype stars', {
+  expect_known_output(brms_SummaryTable(fit1, astrology = TRUE, hype = TRUE, panderize = TRUE, justify = 'lrrrrrrr'),
+                      'brms_pander_output2',
+                      print = TRUE)
+})
+
+test_that('brms_SummaryTable can justify with just hype', {
+  expect_known_output(brms_SummaryTable(fit1, astrology = FALSE, hype = TRUE, panderize = TRUE, justify = 'lrrrrrr'),
+                      'brms_pander_output3',
+                      print = TRUE)
+})
+
+test_that('brms_SummaryTable will error with non-brms object', {
+  expect_error(brms_SummaryTable(lm(mpg ~ wt, mtcars)))
 })
