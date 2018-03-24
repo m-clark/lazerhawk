cbc7cebfd2b753e39c8a6959325c9efdad6f26d1
Michael Clark <micl@umich.edu>
2018-03-24 15:38:40 -0400

    travis issue

--- tests/testthat/test_ggclean.R
+++ tests/testthat/test_ggclean.R
@@ -1,26 +1,20 @@
 context('test visualization themes')
 
+require(ggplot2)  # because travis
+
 test_that('Error returned for non-plotly class for theme_plotly', {
   expect_error(data.frame(x=1:3, y=4:6) %>% theme_plotly())
 })
 
-test_that('Test theme_plotly works', {
+test_that('Check if plotly object returned', {
   expect_s3_class(plotly::plot_ly(x = ~rnorm(100)) %>% theme_plotly(), 'plotly')
 })
 
-test_that('Test theme_blank works', {
+test_that('Check if plotly object returned', {
   expect_s3_class(plotly::plot_ly(x = ~rnorm(100)) %>% theme_blank(), 'plotly')
 })
 
-test_that('Test mode bar for theme_plotly', {
-  expect_s3_class(plotly::plot_ly(x = ~rnorm(100)) %>% theme_plotly(MB=TRUE), 'plotly')
-})
 
-test_that('Test mode bar for theme_blank', {
-  expect_s3_class(plotly::plot_ly(x = ~rnorm(100)) %>% theme_blank(MB=TRUE), 'plotly')
-})
-
-
-test_that('Test theme_trueMinimal', {
+test_that('Check if ggplot object returned', {
   expect_s3_class(ggplot2::qplot(x = rnorm(100)) + theme_trueMinimal(), 'ggplot')
 })
