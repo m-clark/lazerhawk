57d57e4846bfb69bcf73ea649691f51b154ccb00
Michael Clark <micl@umich.edu>
2018-04-14 12:09:24 -0400

    get rid of unnecessary notes

---
+++ R/globals.R
@@ -0,0 +1,3 @@
+# so that check ignores
+utils::globalVariables(c(".", 'Max', 'Mean', 'Median', 'Min', 'Missing', 'N', 'Q1', 'Q3', 'SD',
+'Variable', 'X1st.Qu.', 'X3rd.Qu.', 'perc', 'result', 'results', 'target', 'value', 'x.Freq', 'x.x', 'y.Freq', 'y.x'))
